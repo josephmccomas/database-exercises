9a75480bd76a458f6f37c9859f01b8c213eca9f6
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-03-01 11:05:14 -0600

    Coursework

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE ymir_joseph;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist_name VARCHAR(100) NOT NULL,
+                        album_name  VARCHAR(100) NOT NULL,
+                        release_date INT(4),
+                        sales FLOAT(2),
+                        genre CHAR(255),
+                        PRIMARY KEY (id)
+);
